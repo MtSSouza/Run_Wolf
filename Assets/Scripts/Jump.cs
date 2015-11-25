436e6e10dea31d4e10c14ce222f8a033176d5f63
mathino <mathsouza64@gmail.com>
2015-11-25 17:00:20 -0200

    Add sprites of wolf jump

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+	
+	private bool flyOk;
+	
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		if (coll.gameObject.name == "Platform") 
+		{
+			flyOk = true;
+			this.GetComponent<Animator>().SetBool("toJump", false);
+		}
+	}
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if (coll.gameObject.name == "Platform") 
+		{
+			flyOk = false;
+			this.GetComponent<Animator>().SetBool("toJump", true);
+		}
+	}
+
+	void Pulo()
+	{
+		
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			this.rigidbody2D.AddForce(Vector3.up * 200f);
+		}
+	}
+	
+	void FixedUpdate () 
+	{
+
+		if (flyOk) 
+		{
+			Pulo ();
+		}
+
+	}
+}
