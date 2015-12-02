68a20f6173aa81f368ad40be80850226f46d72cf
mathino <mathsouza64@gmail.com>
2015-12-02 00:30:17 -0200

    Start add scroll speed
    Will need some fixes

---
+++ Assets/Scripts/SpeedChange.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedChange : MonoBehaviour {
+
+    private float speed = 0.01f;
+    private float speedRun;
+    private float speedNow;
+
+	// Use this for initialization
+	void Start () {
+        speedNow = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.GetComponent<Animator>().GetBool("toJump") == true)
+        {
+            this.GetComponent<Animator>().speed = 0.8f;
+        }
+        else
+        {
+            this.GetComponent<Animator>().speed = speedNow;
+            this.GetComponent<Animator>().speed += speedRun;
+        }
+
+
+        speedRun = speed * Time.deltaTime;
+        print(this.GetComponent<Animator>().speed);
+
+	}
+}
