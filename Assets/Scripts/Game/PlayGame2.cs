bc23a2a07abb1fe478a66bbe44361bbb49a73bdf
mathino <mathsouza64@gmail.com>
2015-12-02 05:45:31 -0200

    Add Fade in and out start game

---
+++ Assets/Scripts/Game/PlayGame2.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGame2 : MonoBehaviour {
+
+    private float x;
+    private float y;
+    private float x2;
+    private float y2;
+    private float x3;
+    private float y3;
+    private float x4;
+    private float y4;
+    private float speed = 1f;
+    public GameObject f1;
+    public GameObject f2;
+    public GameObject f3;
+    public GameObject f4;
+
+	// Use this for initialization
+	void Awake () {
+        x = 0.46f;
+        y = 0.46f;
+        x2 = 0.16f;
+        y2 = 0.16f;
+        x3 = 0.05f;
+        y3 = 0.05f;
+        x4 = 0.03f;
+        y4 = 0.03f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        x += speed * Time.deltaTime;
+        y += speed * Time.deltaTime;
+        x2 += speed * Time.deltaTime;
+        y2 += speed * Time.deltaTime;
+        x3 += speed * Time.deltaTime;
+        y3 += speed * Time.deltaTime;
+        x4 += speed * Time.deltaTime;
+        y4 += speed * Time.deltaTime;
+
+        f1.gameObject.transform.localScale = new Vector3(x, y, 0f);
+        f2.gameObject.transform.localScale = new Vector3(x2, y2, 0f);
+        f3.gameObject.transform.localScale = new Vector3(x3, y3, 0f);
+        f4.gameObject.transform.localScale = new Vector3(x4, y4, 0f);
+
+        if (f1.gameObject.transform.localScale.x >= 2.8f)
+        {
+            f1.gameObject.transform.localScale = new Vector3(2.8f, 2.8f, 0f);
+        }
+
+        if (f2.gameObject.transform.localScale.x >= 2.8f)
+        {
+            f2.gameObject.transform.localScale = new Vector3(2.8f, 2.8f, 0f);
+        }
+
+        if (f3.gameObject.transform.localScale.x >= 2.8f)
+        {
+            f3.gameObject.transform.localScale = new Vector3(2.8f, 2.8f, 0f);
+        }
+
+        if (f4.gameObject.transform.localScale.x >= 2.8f)
+        {
+            f4.gameObject.transform.localScale = new Vector3(2.8f, 2.8f, 0f);
+        }
+	}
+}
--- Assets/Scripts/Menu/PlayGame.cs
+++ Assets/Scripts/Menu/PlayGame.cs
@@ -3,13 +3,65 @@ using System.Collections;
 
 public class PlayGame : MonoBehaviour {
 
+    private bool startAll;
+    private bool nextFase;
+    private float x;
+    private float y;
+    private float speed = 4f;
+    public GameObject f1;
+    public GameObject f2;
+    public GameObject f3;
+    public GameObject f4;
+
+
 	// Use this for initialization
 	void Start () {
-	
+        startAll = false;
+        nextFase = false;
+        x = 2.864501f;
+        y = 2.864501f;
 	}
-	
-	// Update is called once per frame
+
+    void OnMouseDown()
+    {
+        startAll = true;
+    }
+
 	void Update () {
-	
+
+        if (startAll)
+        {
+            x -= speed * Time.deltaTime;
+            y -= speed * Time.deltaTime;
+
+            f1.gameObject.transform.localScale = new Vector3(x, y, 0f);
+            f2.gameObject.transform.localScale = new Vector3(x, y, 0f);
+            f3.gameObject.transform.localScale = new Vector3(x, y, 0f);
+            f4.gameObject.transform.localScale = new Vector3(x, y, 0f);
+
+            if (f1.gameObject.transform.localScale.x <= 0.46f) {
+                f1.gameObject.transform.localScale = new Vector3(0.46f, 0.46f, 0f);
+            }
+
+            if (f2.gameObject.transform.localScale.x <= 0.16f)
+            {
+                f2.gameObject.transform.localScale = new Vector3(0.16f, 0.16f, 0f);
+            }
+
+            if (f3.gameObject.transform.localScale.x <= 0.05f)
+            {
+                f3.gameObject.transform.localScale = new Vector3(0.05f, 0.05f, 0f);
+            }
+
+            if (f4.gameObject.transform.localScale.x <= 0.03f)
+            {
+                f4.gameObject.transform.localScale = new Vector3(0.03f, 0.03f, 0f);
+                nextFase = true;
+            }
+        }
+
+        if (nextFase) {
+            //Application.LoadLevel(1);
+        }
 	}
 }
